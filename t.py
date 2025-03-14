d3cc83bd3a61f5e87a088cd4457c26a6a229f18b
NguyenNgocHien <nguyenngochien9464@gmail.com>
2025-03-14 16:31:22 +0700

    done setting and export excel

--- t.py
+++ t.py
@@ -3,24 +3,24 @@
 # watcher._check_files()
 
 # from app.models.base import Base, engine
-from app.models.telegram_message import TelegramMessage
+# from app.models.telegram_message import TelegramMessage
 # from app.models.telegram_users import TelegramUser
-# from app.models.users import User
+from app.models.users import User
 # from app.models.companies import Companies
 # from app.models.notifications import Notification
 # from app.models.tasks import Task
 # Base.metadata.create_all(engine)
-# from config import hash_password
-# from datetime import datetime
+from config import hash_password
+from datetime import datetime
 
-# User.create(
-#     name="Phạm Văn Hùng",
-#     email="supperment",
-#     password=hash_password("hungpv"),
-#     role="admin",
-#     status=True,
-#     avatar="https://example.com/avatar.jpg",
-#     last_login=datetime.utcnow()
-# )
+User.create(
+    name="Nguyễn Ngọc Hiển",
+    email="hiencode",
+    password=hash_password("hiencode"),
+    role="admin",
+    status=True,
+    avatar="https://example.com/avatar.jpg",
+    last_login=datetime.utcnow()
+)
 # # admins = TelegramUser.get(role=1)
 # # print(len(admins))
